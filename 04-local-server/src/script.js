9d66a5acf71e7501dc6b1ae4ae875f75b0fc4c3e
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-01-25 06:12:36 +0300

    adding the axes helper

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -10,14 +10,22 @@ const mesh = new THREE.Mesh(geometry, material)
 // mesh.position.x = 0.7
 // mesh.position.y = - 0.6
 // mesh.position.z = 1
-mesh.position.set(0.7, - 0.6, 1)
+mesh.position.set(0.7, -0.6, 1)
 scene.add(mesh)
 
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+
 // length is the length between the center of the scene and the object
-console.log(mesh.position.length())
+// console.log(mesh.position.length())
+
 
 // will reduce the vector length to be 1
-mesh.position.normalize()
+// mesh.position.normalize()
+
 
 // Sizes
 const sizes = {
@@ -25,14 +33,17 @@ const sizes = {
     height: 600
 }
 
+
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-camera.position.set(1, 1, 3)
+// camera.position.set(1, 1, 3)
 scene.add(camera)
+
 
 // to get the distance from the camera
 console.log(mesh.position.distanceTo(camera.position))
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
