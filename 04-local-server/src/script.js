ac53629d8eee788648fa15b4fa3d46df518721c9
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-01-25 06:32:18 +0300

    Rotation and lookAt

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -7,11 +7,21 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Position
 // mesh.position.x = 0.7
 // mesh.position.y = - 0.6
 // mesh.position.z = 1
 mesh.position.set(0.7, -0.6, 1)
-scene.add(mesh)
+
+// Scale
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation
+mesh.rotation.reorder('YXZ')
+mesh.rotation.y = Math.PI * 0.25
+mesh.rotation.x = Math.PI * 0.25
 
 
 // Axes helper
@@ -40,6 +50,9 @@ camera.position.z = 3
 // camera.position.set(1, 1, 3)
 scene.add(camera)
 
+// Look At
+// camera.lookAt(new THREE.Vector3(0, 0, 0))
+camera.lookAt(mesh.position)
 
 // to get the distance from the camera
 console.log(mesh.position.distanceTo(camera.position))
