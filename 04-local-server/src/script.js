ae441349d65e18c3ad7e4d39b8f5fef7f55aab3f
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-01-26 21:12:54 +0300

    cursor init

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -1,5 +1,15 @@
 import * as THREE from 'three'
-import gsap from 'gsap'
+
+// Cursor
+const cursor = {
+  x: 0,
+  y: 0
+}
+window.addEventListener('mousemove', e => {
+  cursor.x = e.clientX / sizes.width - 0.5
+  cursor.y = -(e.clientY / sizes.height - 0.5)
+  // console.log(cursor)
+})
 
 // Scene
 const scene = new THREE.Scene()
@@ -12,42 +22,57 @@ scene.add(mesh)
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
+  width: 800,
+  height: 600
 }
 
 // Camera
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
 
 const aspectRatio = sizes.width / sizes.height
-const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
-camera.position.set(2,2,2)
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// )
+camera.position.z = 3
+// camera.position.set(2,2,2)
 camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas.webgl')
+  canvas: document.querySelector('canvas.webgl')
 })
 renderer.setSize(sizes.width, sizes.height)
 
 // Clock
 const clock = new THREE.Clock()
 
-
 // Animation
-const tick = () => 
-{
-    // Clock
-    const elapsedTime = clock.getElapsedTime()
-    
-    // Update objects
-    mesh.rotation.y = elapsedTime 
+const tick = () => {
+  // Clock
+  const elapsedTime = clock.getElapsedTime()
 
-    // Render
-    renderer.render(scene, camera)
+  // // Update objects
+  // mesh.rotation.y = elapsedTime
 
-    window.requestAnimationFrame(tick)
+  // Update camera
+  camera.position.set(cursor.x * 3, cursor.y * 3)
+  camera.lookAt(mesh.position)
+
+  // Render
+  renderer.render(scene, camera)
+
+  window.requestAnimationFrame(tick)
 }
 
 tick()
