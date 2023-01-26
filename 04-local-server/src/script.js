415db9174b561e7e5234cf4bcf7c56f59fff7489
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-01-26 16:15:59 +0300

    06 init

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -3,41 +3,11 @@ import * as THREE from 'three'
 // Scene
 const scene = new THREE.Scene()
 
-
-// Objects
-const group = new THREE.Group()
-group.position.y = 1
-group.rotation.y = 1
-scene.add(group)
-
-// Red Cube
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-group.add(cube1)
-
-// Green Cube
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-)
-cube2.position.x = - 2
-group.add(cube2)
-
-// Blue Cube
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x0000ff })
-)
-cube3.position.x = 2
-group.add(cube3)
-
-
-// Axes helper
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
-
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 // Sizes
 const sizes = {
@@ -45,13 +15,10 @@ const sizes = {
     height: 600
 }
 
-
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-// camera.position.set(1, 1, 3)
 scene.add(camera)
-
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
