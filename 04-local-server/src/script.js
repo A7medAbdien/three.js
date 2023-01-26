3be6373bfa6e32e62bb4a420f98e9defe8a8e8a6
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-01-26 21:32:39 +0300

    Orbit Con init

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -66,10 +66,10 @@ const tick = () => {
   // mesh.rotation.y = elapsedTime
 
   // Update camera
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-  camera.position.y = cursor.y * 3
-  camera.lookAt(mesh.position)
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+  // camera.position.y = cursor.y * 3
+  // camera.lookAt(mesh.position)
 
   // Render
   renderer.render(scene, camera)
