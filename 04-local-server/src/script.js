1ace0c5117489adc2c85bbabf807234098f7a2ee
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-01-27 18:44:54 +0300

    07 Full screen done

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -25,6 +25,36 @@ const sizes = {
   height: window.innerHeight
 }
 
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+window.addEventListener('dblclick', () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen()
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen()
+    }
+  } else {
+    if (document.exitFullscreen) document.exitFullscreen()
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen
+  }
+})
+
 // Scene
 const scene = new THREE.Scene()
 
@@ -54,6 +84,7 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // Clock
 const clock = new THREE.Clock()
