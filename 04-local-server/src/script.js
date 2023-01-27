9153977d18a2c0bb68310fb28f4ff64e254ad350
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-01-27 18:08:49 +0300

    07 init, full screen

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -1,3 +1,4 @@
+import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
@@ -20,8 +21,8 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Sizes
 const sizes = {
-  width: 800,
-  height: 600
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
 // Scene
@@ -40,25 +41,13 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 )
-// const aspectRatio = sizes.width / sizes.height
-// const camera = new THREE.OrthographicCamera(
-//   -1 * aspectRatio,
-//   1 * aspectRatio,
-//   1,
-//   -1,
-//   0.1,
-//   100
-// )
 camera.position.z = 3
-// camera.position.set(2,2,2)
 camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true // To enable damping update it on each frame
-// controls.target.y = 2
-// controls.update()
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -76,12 +65,6 @@ const tick = () => {
 
   // // Update objects
   // mesh.rotation.y = elapsedTime
-
-  // Update camera
-  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-  // camera.position.y = cursor.y * 3
-  // camera.lookAt(mesh.position)
 
   // Update Damping
   controls.update()
