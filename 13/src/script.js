bcb49a10f12bfef0269efc33907908242b279d6d
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-01 04:25:18 +0300

    like ili

--- 13/src/script.js
+++ 13/src/script.js
@@ -33,13 +33,13 @@ const fontLoader = new FontLoader()
 
 fontLoader.load(
     '/fonts/helvetiker_regular.typeface.json',
-    (font) =>{
+    (font) => {
         const textGeometry = new TextGeometry(
             myText,
             {
                 font: font,
                 size: 0.5,
-                height: 0.2,
+                height: 0.3,
                 curveSegments: 12,
                 bevelEnabled: true,
                 bevelThickness: 0.03,
@@ -48,55 +48,84 @@ fontLoader.load(
                 bevelSegment: 5
             }
         )
-        // textGeometry.computeBoundingBox()
-        // textGeometry.translate(
-        //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
-        //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
-        //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5
-        // )
         textGeometry.center()
-        
+        // textGeometry.scale(1.5, 1.5, 1.5)
+
         const material = new THREE.MeshNormalMaterial()
-        // material.flatShading = true
-        // const material = new THREE.MeshMatcapMaterial()
-        // material.matcap = matcapTexture
-        
+
         const text = new THREE.Mesh(textGeometry, material)
         scene.add(text)
-        
+
         const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
-        const boxGeometry = new THREE.BoxGeometry(0.5 , 0.5, 0.5)
-        
-        for( let i = 0; i < 100; i++){
-            const donut = new THREE.Mesh(donutGeometry, material)
-            
-            donut.position.x = (Math.random() - 0.5) * 10
-            donut.position.y = (Math.random() - 0.5) * 10
-            donut.position.z = (Math.random() - 0.5) * 10
-            
-            donut.rotation.x = Math.random() * Math.PI
-            donut.rotation.y = Math.random() * Math.PI
-            
-            const scale = Math.random()
-            donut.scale.set(scale, scale, scale)
-            
-            scene.add(donut)
+        const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+        const group = new THREE.Group()
+        scene.add(group)
+
+        const clock = new THREE.Clock()
+
+        const tick = () => {
+            let elapsedTime = clock.getElapsedTime() / 1.5
+
+            // Update Text
+            text.position.y = Math.sin(elapsedTime) * 0.5
+            text.position.x = Math.sin(elapsedTime) * 0.5
+            // text.rotation.x = Math.sin(elapsedTime) * 0.5
+            text.rotation.y = Math.sin(elapsedTime) * 0.5
+
+            // Update Camera
+            camera.position.y = Math.sin(elapsedTime)
+            camera.position.x = Math.sin(elapsedTime)
+
+            elapsedTime = -elapsedTime
+            // Update Group
+            group.position.y = Math.sin(elapsedTime) * 0.5
+            group.position.z = Math.sin(elapsedTime) * 0.5
+            group.rotation.x = Math.sin(elapsedTime) * 0.5
+            group.rotation.z = Math.sin(elapsedTime) * 0.5
+
+
+            // Update controls
+            controls.update()
+
+            // Render
+            renderer.render(scene, camera)
+
+            // Call tick again on the next frame
+            window.requestAnimationFrame(tick)
         }
 
-        for( let i = 0; i < 50; i++){
-            const donut = new THREE.Mesh(boxGeometry, material)
-            
-            donut.position.x = (Math.random() - 0.5) * 10
-            donut.position.y = (Math.random() - 0.5) * 10
-            donut.position.z = (Math.random() - 0.5) * 10
-            
+        tick()
+
+        for (let i = 0; i < 100; i++) {
+            const donut = new THREE.Mesh(donutGeometry, material)
+
+            donut.position.x = ((Math.random() - 0.5) * 15) + 1
+            donut.position.y = ((Math.random() - 0.5) * 15) + 1
+            donut.position.z = ((Math.random() - 0.5) * 15) + 1
+
             donut.rotation.x = Math.random() * Math.PI
             donut.rotation.y = Math.random() * Math.PI
-            
+
             const scale = Math.random()
             donut.scale.set(scale, scale, scale)
-            
-            scene.add(donut)
+
+            group.add(donut)
+        }
+
+        for (let i = 0; i < 50; i++) {
+            const donut = new THREE.Mesh(boxGeometry, material)
+
+            donut.position.x = (Math.random() - 0.5) * 15
+            donut.position.y = (Math.random() - 0.5) * 15
+            donut.position.z = (Math.random() - 0.5) * 15
+
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
+            group.add(donut)
         }
     }
 )
@@ -110,8 +139,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -132,7 +160,7 @@ window.addEventListener('resize', () =>
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 8
 scene.add(camera)
 
 // Controls
@@ -153,14 +181,11 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime() / 2
 
-    // camera.position.y = Math.sin(elapsedTime) * Math.cos(elapsedTime) * 10
-    // camera.position.x = Math.cos(elapsedTime) * 10
     // camera.lookAt(text.position)
-    
+
     // Update controls
     controls.update()
 
