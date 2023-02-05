29e479d3d6e7b8835d21e8d9a8d2e65a15a35326
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-05 21:02:38 +0300

    00 Scroll based, parallax+particles

--- 20-scroll-based-animation/src/script.js
+++ 20-scroll-based-animation/src/script.js
@@ -7,14 +7,31 @@ import * as dat from 'lil-gui'
 const gui = new dat.GUI()
 
 const parameters = {
-    materialColor: '#ffeded'
+    materialColor: '#ffeded',
+    particlesMaterialColor: '#ffeded'
 }
+parameters.particlesSize = 0.03
 
 gui
     .addColor(parameters, 'materialColor')
     .onChange(() => {
-
         material.color.set(parameters.materialColor)
+        particlesMaterial.color.set(parameters.materialColor)
+    })
+
+gui
+    .addColor(parameters, 'particlesMaterialColor')
+    .onChange(() => {
+        particlesMaterial.color.set(parameters.particlesMaterialColor)
+    })
+
+gui
+    .add(parameters, 'particlesSize')
+    .min(0.01)
+    .max(0.1)
+    .step(0.01)
+    .onFinishChange(() => {
+        particlesMaterial.size.set(parameters.particlesSize)
     })
 
 /**
@@ -57,17 +74,36 @@ const mesh3 = new THREE.Mesh(
     material
 )
 
-mesh1.position.y = - objetDistance * 0
-mesh2.position.y = - objetDistance * 1
-mesh3.position.y = - objetDistance * 2
-
-scene.add(mesh1, mesh2, mesh3)
-
 const sectionMeshes = [mesh1, mesh2, mesh3]
 
 for (let i = 0; i < sectionMeshes.length; i++) {
     sectionMeshes[i].position.x = (i % 2) ? -2 : 2
+    sectionMeshes[i].position.y = - objetDistance * i
 }
+
+scene.add(mesh1, mesh2, mesh3)
+
+/**
+ * Particles
+ */
+const particlesCount = 200
+const positions = new Float32Array(particlesCount * 3)
+for (let i = 0; i < positions.length; i++) {
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+    positions[i * 3 + 1] = objetDistance * 0.5 - Math.random() * objetDistance * sectionMeshes.length
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+}
+
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.particlesMaterialColor,
+    sizeAttenuation: true,
+    size: parameters.particlesSize
+})
+const particlesMesh = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particlesMesh)
 
 /**
  * Lights
@@ -101,10 +137,14 @@ window.addEventListener('resize', () => {
 /**
  * Camera
  */
+// Group
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
@@ -127,15 +167,36 @@ window.addEventListener('scroll', () => {
 })
 
 /**
+ * Cursor
+ */
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - .5
+    cursor.y = - (event.clientY / sizes.width - .5)
+})
+
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
+let oldElapsedTime = 0
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldElapsedTime
+    oldElapsedTime = elapsedTime
 
     // Animate Camera
     camera.position.y = - (scrollY / sizes.height) * objetDistance
+
+    const parallaxX = cursor.x * 0.5
+    const parallaxY = cursor.y * 0.5
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * deltaTime * 5
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * deltaTime * 5
 
     // Animate Meshes
     for (const mesh of sectionMeshes) {
