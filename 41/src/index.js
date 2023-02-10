fca60ab5e786902f8731868abae14a1b36bc8dfb
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-10 12:45:30 +0300

    42 R3F, torusKnot from an Experience Component

--- 41/src/index.js
+++ 41/src/index.js
@@ -1,7 +1,12 @@
+import "./style.css"
+import { Canvas } from '@react-three/fiber'
 import { createRoot } from 'react-dom/client'
+import Experience from "./Experience"
 
 const root = createRoot(document.querySelector('#root'))
 
 root.render(
-    <>Hello React</>
+    <Canvas>
+        <Experience />
+    </Canvas>
 )
