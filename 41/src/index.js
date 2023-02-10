0499d96a9f9383d9e4c7e224997003a88e0d3f17
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-10 14:36:36 +0300

    42 R3F, Access camera

--- 41/src/index.js
+++ 41/src/index.js
@@ -6,7 +6,14 @@ import Experience from "./Experience"
 const root = createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas>
+    <Canvas
+        camera={{
+            fov: 45,
+            near: 1,
+            far: 200,
+            position: [3, 2, 6]
+        }}
+    >
         <Experience />
     </Canvas>
 )
