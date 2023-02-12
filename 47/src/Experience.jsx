d8e40c9ed0befd3c904fda542549b295fb49415f
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-12 05:13:41 +0300

    47 3D, meshMatcapMaterial

--- 47/src/Experience.jsx
+++ 47/src/Experience.jsx
@@ -1,16 +1,31 @@
-import { OrbitControls, Text3D } from '@react-three/drei'
+import { Center, OrbitControls, Text3D, useMatcapTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 
 export default function Experience() {
+
+    const [matcapTexture] = useMatcapTexture('6E524D_8496C5_AF6624_100B11', 256)
+
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <Text3D font={'/fonts/helvetiker_regular.typeface.json'}>
-            I aM NoT hErE
-            <meshNormalMaterial />
-        </Text3D>
+        <Center>
+            <Text3D
+                font={'/fonts/helvetiker_regular.typeface.json'}
+                size={0.75}
+                height={0.2}
+                curveSegments={12}
+                bevelEnabled
+                bevelThickness={0.02}
+                bevelSize={0.02}
+                bevelOffset={0}
+                bevelSegments={5}
+            >
+                GiMMy
+                <meshMatcapMaterial matcap={matcapTexture} />
+            </Text3D>
+        </Center>
     </>
 }
