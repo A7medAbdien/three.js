ecc16918e3469606ef47ca3537f747f0ed8dd854
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-13 02:28:30 +0300

    48 Portal, chang BG

--- 48/src/Experience.jsx
+++ 48/src/Experience.jsx
@@ -1,12 +1,12 @@
 import { OrbitControls } from '@react-three/drei'
 
-export default function Experience()
-{
+export default function Experience() {
     return <>
+        <color args={['#201919']} attach="background" />
 
         <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 }>
+        <mesh scale={1.5}>
             <boxGeometry />
             <meshNormalMaterial />
         </mesh>
