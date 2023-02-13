890e7a5d7eaaa806631f76800b13d364787c4758
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-14 00:56:55 +0300

    51 portfolio, add Mac

--- 51-portfolio/src/Experience.jsx
+++ 51-portfolio/src/Experience.jsx
@@ -1,15 +1,23 @@
-import { OrbitControls } from '@react-three/drei'
+import { Environment, OrbitControls, useGLTF } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
 
-export default function Experience()
-{
+export default function Experience() {
+    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+
     return <>
 
+        <Perf position='top-left' />
+
+        <Environment preset='city' />
+
+        <color args={['#695b5b']} attach="background" />
+
         <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+
+        <primitive
+            object={computer.scene}
+            position-y={- 1.2}
+        />
 
     </>
 }
