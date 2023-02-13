f20b083b98606005d26ab711dd4e954a5b4dff6f
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-13 07:28:47 +0300

    50 PP, bloom

--- 50-post/src/Experience.jsx
+++ 50-post/src/Experience.jsx
@@ -1,28 +1,34 @@
 import { OrbitControls } from '@react-three/drei'
+import { Bloom, EffectComposer } from '@react-three/postprocessing'
 import { Perf } from 'r3f-perf'
 
-export default function Experience()
-{
+export default function Experience() {
     return <>
+
+        <color args={['#000000']} attach="background" />
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+        <EffectComposer>
+            <Bloom mipmapBlur />
+        </EffectComposer>
 
-        <mesh castShadow position-x={ - 2 }>
+        <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+        <ambientLight intensity={0.5} />
+
+        <mesh castShadow position-x={- 2}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
+        <mesh castShadow position-x={2} scale={1.5}>
             <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
+            <meshStandardMaterial color={[1.5, 1, 4]} toneMapped={false} />
         </mesh>
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh receiveShadow position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
