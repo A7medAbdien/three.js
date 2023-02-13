7d2081b3ead8b1060a025c1e6674ea44b8c3287b
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-14 01:36:16 +0300

    51 portfolio, rectAreaLight

--- 51-portfolio/src/Experience.jsx
+++ 51-portfolio/src/Experience.jsx
@@ -24,6 +24,14 @@ export default function Experience() {
                     object={computer.scene}
                     position-y={- 1.2}
                 >
+                    <rectAreaLight
+                        width={2.5}
+                        height={1.65}
+                        intensity={65}
+                        color={'#ff6900'}
+                        rotation={[0.1, Math.PI, 0]}
+                        position={[0, 0.55, -1.15]}
+                    />
                     <Html
                         transform
                         wrapperClass='htmlScreen'
