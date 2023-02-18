a7f69a5fe0937d3f116fdf9fd5ef1cf021fa2bdb
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-18 12:36:31 +0300

    first part

--- starter/src/Experience.jsx
+++ starter/src/Experience.jsx
@@ -1,14 +1,55 @@
 import { OrbitControls } from '@react-three/drei'
+import { useControls } from 'leva'
 
-export default function Experience()
-{
+export default function Experience() {
+
+    const { radius, tube, radialSeg, tubSeg, z } = useControls('', {
+        z: {
+            value: 2.3,
+            step: 0.01,
+            min: 0,
+            max: 3
+        },
+        radius: {
+            value: 1,
+            step: 0.1,
+            min: 0,
+            max: 3
+        },
+        tube: {
+            value: 0.8,
+            step: 0.01,
+            min: 0,
+            max: 5
+        },
+        radialSeg: {
+            value: 30,
+            step: 1,
+            min: 5,
+            max: 100
+        },
+        tubSeg: {
+            value: 30,
+            step: 1,
+            min: 5,
+            max: 100
+        },
+    })
+
     return <>
 
         <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
+
+        <axesHelper args={[5, 5, 5]} />
+
+        <mesh
+            scale-z={z}
+        >
+            <torusGeometry args={[radius, tube, radialSeg, tubSeg]} />
+            <meshNormalMaterial
+                color="red"
+            // wireframe
+            />
         </mesh>
 
     </>
--- starter/src/index.jsx
+++ starter/src/index.jsx
@@ -7,12 +7,12 @@ const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas
-        camera={ {
+        camera={{
             fov: 45,
             near: 0.1,
             far: 2000,
-            position: [ -3, 1.5, 4 ]
-        } }
+            // position: [ -3, 1.5, 4 ]
+        }}
     >
         <Experience />
     </Canvas>
