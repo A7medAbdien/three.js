e69b222ed8026c46d0647a1810283596ec2d5fe6
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-24 18:38:26 +0300

    add Cap comp done

---
+++ rope/src/Cap.jsx
@@ -0,0 +1,77 @@
+import { QuadraticBezierLine } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { RigidBody, useSphericalJoint } from "@react-three/rapier";
+import { useRef } from "react";
+import { Vector3 } from "three";
+
+
+const RopeJoint = ({ a, b }) => {
+    useSphericalJoint(a, b, [
+        [0, 0.5, 0],
+        [-0.05, 0, 0]
+    ]);
+    return null;
+};
+
+function Cable({ start, end, v1 = new Vector3(), v2 = new Vector3() }) {
+    const ref = useRef()
+    useFrame(() => {
+        const startPoint = start.current.getWorldPosition(v1)
+        const endPoint = end.current.getWorldPosition(v2)
+        endPoint.y += 0.08
+        const midPoint = new Vector3(
+            startPoint.x * 1.1,
+            startPoint.y * 1.1,
+            startPoint.z * 1.1)
+        ref.current.setPoints(startPoint, endPoint, midPoint)
+    }, [])
+    return <QuadraticBezierLine ref={ref} lineWidth={3} color="black" />
+}
+
+export const Cap = ({ anchor, free }) => {
+    const { midAnchor, midAnchorMesh, midAnchorNode } = anchor
+    const { freeCap, freeCapMesh, freeCapNode } = free
+
+
+    useFrame(() => {
+        const pos = new Vector3()
+        midAnchorMesh.current.getWorldPosition(pos)
+        midAnchor.current.setTranslation(new Vector3(
+            pos.x,
+            pos.y,
+            pos.z,
+        ))
+    })
+
+    return (
+        <>
+            <RigidBody ref={midAnchor} colliders={"ball"} type={"kinematicPosition"}>
+                <group ref={midAnchorMesh} position={midAnchorNode.position} />
+            </RigidBody >
+
+            <RigidBody ref={freeCap} type={"dynamic"}>
+                <group ref={freeCapMesh}>
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={freeCapNode.small.geometry}
+                        material={freeCapNode.small.material}
+                        position={[0, -0.17, 0]}
+                        rotation={[-0.03, 0, 0]}
+                        scale={0.42}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={freeCapNode.big.geometry}
+                        material={freeCapNode.big.material}
+                        rotation={[0.03, 0, 0]}
+                        scale={0.69}
+                    />
+                </group>
+            </RigidBody >
+            <RopeJoint a={freeCap} b={midAnchor} radius={1.7} loss={0} />
+            <Cable start={midAnchorMesh} end={freeCapMesh} />
+        </>
+    )
+}
--- rope/src/Experience.jsx
+++ rope/src/Experience.jsx
@@ -22,6 +22,7 @@ import {
 
 import "./style.css";
 import { Rope } from "./Rope";
+import { Cap } from "./Cap";
 import { Perf } from "r3f-perf";
 import { useControls } from "leva";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
@@ -177,80 +178,5 @@ export default function Experience() {
         </>
     );
 }
-
-const Cap = ({ anchor, free }) => {
-    const { midAnchor, midAnchorMesh, midAnchorNode } = anchor
-    const { freeCap, freeCapMesh, freeCapNode } = free
-
-
-    const RopeJoint = ({ a, b }) => {
-        useSphericalJoint(a, b, [
-            [0, 0.5, 0],
-            [-0.05, 0, 0]
-        ]);
-        return null;
-    };
-
-    function Cable({ start, end, v1 = new Vector3(), v2 = new Vector3() }) {
-        const ref = useRef()
-        useFrame(() => {
-            const startPoint = start.current.getWorldPosition(v1)
-            const endPoint = end.current.getWorldPosition(v2)
-            endPoint.y += 0.08
-            const midPoint = new Vector3(
-                startPoint.x * 1.1,
-                startPoint.y * 1.1,
-                startPoint.z * 1.1)
-            ref.current.setPoints(startPoint, endPoint, midPoint)
-        }, [])
-        return <QuadraticBezierLine ref={ref} lineWidth={3} color="black" />
-    }
-
-    useFrame(() => {
-        const pos = new Vector3()
-        midAnchorMesh.current.getWorldPosition(pos)
-        midAnchor.current.setTranslation(new Vector3(
-            pos.x,
-            pos.y,
-            pos.z,
-        ))
-    })
-
-    return (
-        <>
-            <RigidBody ref={midAnchor} colliders={"ball"} type={"kinematicPosition"}>
-                <group ref={midAnchorMesh} position={midAnchorNode.position} />
-            </RigidBody >
-
-            <RigidBody ref={freeCap} type={"dynamic"}>
-                <group ref={freeCapMesh}>
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={freeCapNode.small.geometry}
-                        material={freeCapNode.small.material}
-                        position={[0, -0.17, 0]}
-                        rotation={[-0.03, 0, 0]}
-                        scale={0.42}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={freeCapNode.big.geometry}
-                        material={freeCapNode.big.material}
-                        rotation={[0.03, 0, 0]}
-                        scale={0.69}
-                    />
-                </group>
-            </RigidBody >
-            <RopeJoint a={freeCap} b={midAnchor} radius={1.7} loss={0} />
-            <Cable start={midAnchorMesh} end={freeCapMesh} />
-        </>
-    )
-
-
-
-}
-
 
 useGLTF.preload("/boxWithSb7a.glb");
